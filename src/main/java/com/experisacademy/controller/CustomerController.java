c8f16124bd7def25f4ba627c2ac7ed09fc3c2635
Robin Eliasson <robineliasson93@gmail.com>
2021-07-20 10:25:06 +0200

    Added dao interaction + service class. Fixed up some of the SQL, fixed test that broken.

--- src/main/java/com/experisacademy/controller/CustomerController.java
+++ src/main/java/com/experisacademy/controller/CustomerController.java
@@ -1,8 +1,13 @@
 package com.experisacademy.controller;
 
+import com.experisacademy.model.Customer;
+import com.experisacademy.model.CustomerCountry;
+import com.experisacademy.model.CustomerSpender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.experisacademy.service.CustomerService;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -15,32 +20,54 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    /*@GetMapping("/customers")
-    public ArrayList<Customer> getAllDays() {
-        return customerService.getAllDays();
-    }
-
     @GetMapping("/id/{id}")
-    public Customer getSpecificCustomerById(@PathVariable String id) {
+    public Customer searchByCustomerId(@PathVariable long id){
         return customerService.getCustomerById(id);
     }
-
-    @GetMapping("/name/{name}")
-    public Customer getSpecificCustomerByName(@PathVariable String name) {
-        return customerService.getCustomerByName(name);
+    //Requests customer details based upon customer name
+    @GetMapping("/name/{firstName}")
+    public ArrayList<Customer> searchByCustomerName(@PathVariable String firstName){
+        return customerService.getCustomerByName(firstName);
     }
+    //Requests a list of all customers
+    @GetMapping("/customers")
+    public ArrayList<Customer> showAllCustomers(){
+        return customerService.getCustomers();
+    }
+    //Requests the amount of customers registered to each country, then returns a list of those countries in descending order
+    @GetMapping("/customers/country")
+    public ArrayList<CustomerCountry> showCustomerOrdersByCountry() {
+        return customerService.getCountriesByOrderCount();
+    }
+    //Requests a an ordered list of customers whom spend the most, in descending order
+    @GetMapping("/customers/spending")
+    public ArrayList<CustomerSpender> showCustomersOrderedBySpending() {
+        return customerService.getCustomerSpender();
 
+    }
+    //Requests a subset of customers based upon limit and offset
+    @GetMapping("/customers/subset/{limit}/{offset}")
+    public ArrayList<Customer> getSubsetCustomers(@PathVariable int limit,
+                                                     @PathVariable int offset) {
+        return customerService.getCustomerSubset(limit, offset);
+
+    }
+    //Requests a specific customers favourite genre along with their normal details, by customer Id
+    /*@GetMapping("/api/getCustomerByIdShowFavouriteGenre")
+    public String searchByCustomerNameFavouriteGenre(@RequestParam("query")String terms){
+        CustomerGenre customer = customerDataAccessService.specificCustomerPopularGenre(terms);
+        return customerToStringGenre(customer);
+    }*/
+    //Posts a new customer
     @PostMapping
-    public boolean addCustomer(@RequestBody Customer customer) {
+    public Boolean createCustomer(@RequestBody Customer customer){
         return customerService.addCustomer(customer);
     }
-
-    @PutMapping("{id}")
-    public boolean updateCustomer(@PathVariable String id,
-                                  @NonNull @RequestBody Customer customer) {
-        return false;
-
-    }*/
+    //Updates a new customer
+    @PutMapping("id/{id}")
+    public Boolean updateCustomer(@PathVariable long id, @RequestBody Customer customer ){
+        return customerService.updateCustomer(id, customer);
+    }
 
 
 
