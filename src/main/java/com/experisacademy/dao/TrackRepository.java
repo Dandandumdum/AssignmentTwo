5916ea8cb41a65b1841a68c99c90c134e38c5c2b
Daniel Dumville <danieldumville@gmail.com>
2021-07-21 15:44:07 +0200

    Added SQL for search track

--- src/main/java/com/experisacademy/dao/TrackRepository.java
+++ src/main/java/com/experisacademy/dao/TrackRepository.java
@@ -38,6 +38,33 @@ public class TrackRepository implements TrackDao {
     }
     @Override
     public Track selectTrack(String trackName) {
+        ArrayList<String> song = new ArrayList<>();
+
+        try(Connection con = DriverManager.getConnection(ConnectionHelper.getConnectionURL())) {
+            System.out.println("Connection to SQLite has been established.");
+
+            PreparedStatement preparedStatement = con.prepareStatement(
+                    "SELECT Track.Name, Artist.Name, Album.Title, Genre.Name FROM Track\n" +
+                            "INNER JOIN Album  on Track.AlbumId = Album.AlbumId\n" +
+                            "INNER JOIN Artist  on Album.ArtistId = Artist.ArtistId\n" +
+                            "INNER JOIN Genre on Track.GenreId = Genre.GenreId\n" +
+                            "WHERE Track.Name LIKE ?");
+            preparedStatement.setString(1, trackName);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                song.add(resultSet.getString("Track.Name"));
+                song.add(resultSet.getString("Artist.Name"));
+                song.add(resultSet.getString("Album.Title"));
+                song.add(resultSet.getString("Genre.Name"));
+            }
+
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+
         return null;
     }
 }
