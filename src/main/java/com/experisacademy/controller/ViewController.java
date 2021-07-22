c99427f008bd5e9123834b04599e26fe1470ee81
Robin Eliasson <robineliasson93@gmail.com>
2021-07-22 13:37:28 +0200

    "Finished" home page. Completed required functionality for displaying information on respective page.

--- src/main/java/com/experisacademy/controller/ViewController.java
+++ src/main/java/com/experisacademy/controller/ViewController.java
@@ -5,11 +5,11 @@ import com.experisacademy.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/api/v1/tracks")
@@ -18,7 +18,7 @@ public class ViewController {
     private static final String FIRST_TABLE = "Artist";
     private static final String SECOND_TABLE = "Track";
     private static final String THIRD_TABLE = "Genre";
-    private static final int LIMIT = 5;
+    private static final int LIMIT = 6;
 
     private TrackService trackService;
 
@@ -33,14 +33,23 @@ public class ViewController {
         model.addAttribute("songs", trackService.getSongNames(SECOND_TABLE, LIMIT));
         model.addAttribute("genres", trackService.getGenreNames(THIRD_TABLE, LIMIT));
 
-        model.addAttribute("greeting", "Welcome to the past with Thymeleaf");
         return "home";
     }
 
     @GetMapping("/results")
     public String displayResults(Model model, @RequestParam String name) {
+        name = capitalizeEachWord(name);
         model.addAttribute("name", trackService.getTrack(name));
         return "results";
+    }
+
+    private String capitalizeEachWord(String sentence) {
+        String formattedName = "";
+        String words[] = sentence.trim().replaceAll(" +", " ").split(" ");
+        for(String word : words) {
+            formattedName += word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase() + " ";
+        }
+        return formattedName.trim();
     }
 
 
